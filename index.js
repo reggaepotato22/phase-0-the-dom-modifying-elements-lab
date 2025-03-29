7753afa8fd5856cb1bee6bea2de09e4ebef04153
Andrew <andrewmandieka@gmail.com>
2025-03-29 20:33:58 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById("main").remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Your-Name is the champion"; // Replace "Your-Name" with your actual name
+
+document.body.appendChild(newHeader);
